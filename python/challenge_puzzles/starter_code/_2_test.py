35f6e5829cfd365730a4089817cc7eb5cfff34a0
7006 <22399553+7006@users.noreply.github.com>
2025-11-23 10:30:11 +0200

    Update _2_test.py

--- python/challenge_puzzles/starter_code/_2_test.py
+++ python/challenge_puzzles/starter_code/_2_test.py
@@ -3,14 +3,14 @@ from _2 import *
 
 
 @pytest.mark.parametrize(
-    "num_one, num_two, num_sum",
+    "num_sum, num_one, num_two",
     [
-        (20, 20, 40),
-        (20, 30, None),
-        (20, 100, None),
+        (40, 20, 20),
+        (None, 20, 30),
+        (None, 20, 100),
     ],
 )
 def test_sum_if_less_than_fifty(
-    num_one: int, num_two: int, num_sum: int | None
+    num_sum: int | None, num_one: int, num_two: int
 ) -> None:
-    assert sum_if_less_than_fifty(num_one, num_two) == num_sum
+    assert num_sum == sum_if_less_than_fifty(num_one, num_two)
