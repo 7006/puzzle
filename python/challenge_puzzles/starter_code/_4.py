83eba646355108838ab9789c5a6f7378f5de505b
7006 <alexbox86@gmail.com>
2025-11-23 04:31:39 +0200

    _4

--- python/challenge_puzzles/starter_code/_4.py
+++ python/challenge_puzzles/starter_code/_4.py
@@ -1,2 +1,10 @@
+VOWELS = ["a", "e", "i", "o", "u"]
+
+
 def remove_vowels(input_str: str) -> str:
-    pass
+    chars = [char for char in input_str if not is_vowel(char)]
+    return "".join(chars)
+
+
+def is_vowel(char: str) -> bool:
+    return char.lower() in VOWELS
--- python/challenge_puzzles/starter_code/_4_test.py
+++ python/challenge_puzzles/starter_code/_4_test.py
@@ -3,12 +3,12 @@ from _4 import *
 
 
 @pytest.mark.parametrize(
-    "input_str, output_str",
+    "output_str, input_str",
     [
         ("Hll, Wrld!", "Hello, World!"),
         ("", "aeiouAEIOU"),
         ("zzxxxccvvvbbnnmmmLLKKJJHH", "zzxxxccvvvbbnnmmmLLKKJJHH"),
     ],
 )
-def test_remove_vowels(input_str: str, output_str: str) -> None:
-    assert remove_vowels(input_str) == output_str
+def test_remove_vowels(output_str: str, input_str: str) -> None:
+    assert output_str == remove_vowels(input_str)
