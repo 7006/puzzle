c4d21ab1334184655f7a09f2745c7b56b3eb0e6a
7006 <alexbox86@gmail.com>
2025-11-24 23:56:30 +0200

    6 sol

--- erlang/src/p6.erl
+++ erlang/src/p6.erl
@@ -2,4 +2,9 @@
 -export([filter_even_length_strings/1]).
 
 filter_even_length_strings(InputStrs) ->
-    InputStrs.
+    [I || I <- InputStrs, len(I) rem 2 =:= 0].
+
+len(Str) when is_list(Str) ->
+    erlang:length(Str);
+len(Bin) when is_binary(Bin) ->
+    erlang:byte_size(Bin).
--- erlang/test/p6_test.erl
+++ erlang/test/p6_test.erl
@@ -3,7 +3,44 @@
 
 filter_even_length_strings_test_() ->
     [
-        ?_assertEqual(["fish", "elephant"], ["cat", "dog", "fish", "elephant"]),
-        ?_assertEqual([], ["q", "w", "e", "r", "t", "y"]),
-        ?_assertEqual(["qq", "ww", "ee", "rr", "yy"], ["qq", "ww", "ee", "rr", "t", "yy"])
+        ?_assertEqual(
+            ["fish", "elephant"],
+            p6:filter_even_length_strings([
+                "cat", "dog", "fish", "elephant"
+            ])
+        ),
+        ?_assertEqual(
+            [],
+            p6:filter_even_length_strings([
+                "q", "w", "e", "r", "t", "y"
+            ])
+        ),
+        ?_assertEqual(
+            ["qq", "ww", "ee", "rr", "yy"],
+            p6:filter_even_length_strings([
+                "qq", "ww", "ee", "rr", "t", "yy"
+            ])
+        )
+    ].
+
+filter_even_length_binary_strings_test_() ->
+    [
+        ?_assertEqual(
+            [<<"fish">>, <<"elephant">>],
+            p6:filter_even_length_strings([
+                <<"cat">>, <<"dog">>, <<"fish">>, <<"elephant">>
+            ])
+        ),
+        ?_assertEqual(
+            [],
+            p6:filter_even_length_strings([
+                <<"q">>, <<"w">>, <<"e">>, <<"r">>, <<"t">>, <<"y">>
+            ])
+        ),
+        ?_assertEqual(
+            [<<"qq">>, <<"ww">>, <<"ee">>, <<"rr">>, <<"yy">>],
+            p6:filter_even_length_strings([
+                <<"qq">>, <<"ww">>, <<"ee">>, <<"rr">>, <<"t">>, <<"yy">>
+            ])
+        )
     ].
